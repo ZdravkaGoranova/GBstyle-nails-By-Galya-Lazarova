1ac61127fd7561c4fbf554df15b6927133ae0d8e
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-04-21 11:39:45 +0300

    Add photos

---
+++ my-app/src/components/Home/Home.js
@@ -0,0 +1,27 @@
+
+
+
+import React, { useContext } from "react";
+
+
+//import Spinner from '../Spinner/Spinner.js';
+
+export default function Home() {
+
+
+    return (
+        <>
+            <section id="home-page">
+                <article className="auto-content">
+                    <h1>Wood Woord Bulgaria</h1>
+                </article>
+            </section>
+            <section id="home-page-content">
+                <h1>Latest Products publication</h1>
+
+          
+            </section>
+        </>
+    )
+}
+
