7aee6833d3db24cd103180761fd731e27055b94b
ZdravkaGoranova <zdr_mladenova90@abv.bg>
2023-11-22 16:03:27 +0200

    Update category

--- my-app/src/components/Category/Category.js
+++ my-app/src/components/Category/Category.js
@@ -6,8 +6,6 @@ export default function Category() {
 
     return (
         <>
-          
-
             <h2 className="text-center fs-1 fst-italic" >Beauty Salon -by Galya Lazarova</h2>
 
             <figure className="text-center" id='aboutUs'>
@@ -16,163 +14,107 @@ export default function Category() {
             </figure>
 
             <div class="container">
+            {/* <div className="row">
+                    <div className="col-md-6 mb-4"> */}
+              
+                <ul class="row row-cols-1 row-cols-md-6 g-8 hash-list  pad-30-all align-center text-sm">
+                    <li >
+                        <img src="img/viber_image_logo.jpg" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
 
-                <div class="row position-relative mt-n1-9">
-                    <div class="col-md-6 col-lg-4 mt-1-9">
-                        <div class="team-style1 text-center">
-                            <img src="img/viber_image_2023-04-21_17-31-48-863.jpg" style={{ width: '480px', height: '480px' }} class="border-radius-5" alt="..." />
-                            {/* <img src="img/viber_image_2023-04-21_17-31-48-863.jpg" className="card-img-top" alt="..." style={{ marginTop: "20px" }} />
-                     */}
-                            <div class="team-info">
-                                <h3 class=" mb-1 h4">Професионални курсове</h3>
+                        <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Професионални курсове</h5>
+                        <p class="fs-110 font-cond-l" contenteditable="false">
+                            <p class=" mb-0">
+                                <ul>
+                                    <li> Маникюр</li>
+                                    <li>Педикюр</li>
+                                    <li>Hоктопластика</li>
+                                </ul>
+                            </p></p>
+                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    </li>
 
-                            </div>
-                            <div class="team-overlay">
-                                <div class="d-table h-100 w-100">
-                                    <div class="d-table-cell align-middle">
-                                        <h3><a href="#" class="text-white">Професионални курсове</a></h3>
-                                        <p class="text-white mb-0">
-                                            <ul>
-                                                <li> Маникюр</li>
-                                                <li>Педикюри</li>
-                                                <li>Hоктопластика</li>
-                                            </ul>
-                                        </p>
-                                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    <li >
+                        <img src="img/viber_image_logo.jpg" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
 
-                                        {/* <ul class="social-icon-style1">
-                                            <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
-                                            <li><a href="#!"><i class="fab fa-twitter"></i></a></li>
-                                            <li><a href="#!"><i class="fab fa-youtube"></i></a></li>
-                                            <li><a href="#!"><i class="fab fa-linkedin-in"></i></a></li>
-                                        </ul> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mt-1-9">
-                        <div class="team-style1 text-center">
-                            <img src="img/viber_image_2023-04-21_17-53-59-167.jpg" style={{ width: '480px', height: '480px' }} class="border-radius-5" alt="..." />
-                            <div class="team-info">
-                                <h3 class=" mb-1 h4">Грижа за ноктите</h3>
+                        <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Грижа за лицето</h5>
+                        <p class="fs-110 font-cond-l" contenteditable="false">
+                            <p class=" mb-0">
+                                <ul>
+                                    <li> HYDRA BEATY терапия</li>
+                                    <li>диамантено дермаабразио</li>
+                                    <li>BB Glow терапия</li>
+                                    <li>микронидлинг</li>
+                                </ul>
+                            </p></p>
+                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    </li>
 
-                            </div>
-                            <div class="team-overlay">
-                                <div class="d-table h-100 w-100">
-                                    <div class="d-table-cell align-middle">
-                                        <h3><a href="#" class="text-white">Грижа за ноктите</a></h3>
-                                        <p class="text-white mb-0"><ul>   <li> Маникюр</li>
-                                            <li>Педикюри</li>
-                                            <li>Hоктопластика</li></ul></p>
-                                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mt-1-9">
-                        <div class="team-style1 text-center">
-                            <img src="img/287318034_3173677216238121_3876630277965791557_n.jpg" style={{ width: '480px', height: '480px' }} class="border-radius-5" alt="..." />
-                            <div class="team-info">
-                                <h3 class=" mb-1 h4">Грижа за лицето</h3>
+                    <li >
+                        <img src="img/viber_image_logo.jpg" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
 
-                            </div>
-                            <div class="team-overlay">
-                                <div class="d-table h-100 w-100">
-                                    <div class="d-table-cell align-middle">
-                                        <h3><a href="#" class="text-white">Грижа за лицето</a></h3>
-                                        <p class="text-white mb-0"><ul>   <li> HYDRA BEATY терапия</li>
-                                            <li>диамантено дермаабразио</li>
-                                            <li>BB Glow терапия</li>
-                                            <li>микронидлинг</li></ul>
-                                        </p>
-                                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mt-1-9">
-                        <div class="team-style1 text-center">
-                            <img src="img/89376124_2554953618110487_1109544576890175488_n.jpg" style={{ width: '480px', height: '480px' }} class="border-radius-5" alt="..." />
-                            {/* <img src="img/viber_image_2023-04-21_17-31-48-863.jpg" className="card-img-top" alt="..." style={{ marginTop: "20px" }} />
-                     */}
-                            <div class="team-info">
-                                <h3 class=" mb-1 h4">Професионален грим</h3>
+                        <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Грижа за ноктите</h5>
+                        <p class="fs-110 font-cond-l" contenteditable="false">
+                            <p class=" mb-0">
+                                <ul>
+                                    <li> Маникюр</li>
+                                    <li>Педикюр</li>
+                                    <li>Hоктопластика</li>
+                                </ul>
+                            </p></p>
+                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    </li>
+                    
+                    <li >
+                        <img src="img/viber_image_logo.jpg" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
 
-                            </div>
-                            <div class="team-overlay">
-                                <div class="d-table h-100 w-100">
-                                    <div class="d-table-cell align-middle">
-                                        <h3><a href="#" class="text-white">Професионален грим</a></h3>
-                                        <p class="text-white mb-0">
-                                            <ul>
-                                                <li> Ежедневен</li>
-                                                <li>Вечерен</li>
-                                                <li>Официален</li>
-                                                <li>Грим за камера</li>
-                                                <li>Сценичен</li>
-                                                <li>Уроци по самогримиране</li>
-                                                <li>Курсове по грим</li>
-                                            </ul>
-                                        </p>
-                                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                        <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Професионален грим</h5>
+                        <p class="fs-110 font-cond-l" contenteditable="false">
+                            <p class=" mb-0">
+                                <ul>
+                                    <li> Ежедневен</li>
+                                    <li>Вечерен</li>
+                                    <li>Официален</li>
+                                    <li>Грим за камера</li>
+                                    <li>Сценичен</li>
+                                    <li>Уроци по самогримиране</li>
+                                    <li>Курсове по грим</li>
+                                </ul>
+                            </p></p>
+                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    </li>
 
-                                        {/* <ul class="social-icon-style1">
-                                            <li><a href="#!"><i class="fab fa-facebook-f"></i></a></li>
-                                            <li><a href="#!"><i class="fab fa-twitter"></i></a></li>
-                                            <li><a href="#!"><i class="fab fa-youtube"></i></a></li>
-                                            <li><a href="#!"><i class="fab fa-linkedin-in"></i></a></li>
-                                        </ul> */}
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mt-1-9">
-                        <div class="team-style1 text-center">
-                            <img src="img/86180368_2532503963688786_4443046249263792128_n.jpg" style={{ width: '480px', height: '480px' }} class="border-radius-5" alt="..." />
-                            <div class="team-info">
-                                <h3 class=" mb-1 h4">Масажи</h3>
+                    <li >
+                        <img src="img/viber_image_logo.jpg" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
 
-                            </div>
-                            <div class="team-overlay">
-                                <div class="d-table h-100 w-100">
-                                    <div class="d-table-cell align-middle">
-                                        <h3><a href="#" class="text-white">Масажи</a></h3>
-                                        <p class="text-white mb-0"><ul>   <li> Маникюр</li>
-                                            <li> Калсически</li>
-                                            <li>Силов</li>
-                                            <li>Възстановителен</li>
-                                            <li>Антицелулитен масаж с вендузи</li></ul></p>
-                                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-lg-4 mt-1-9">
-                        <div class="team-style1 text-center">
-                            <img src="img/viber_image_2023-04-26_13-54-21-767.jpg" style={{ width: '480px', height: '480px' }} class="border-radius-5" alt="..." />
-                            <div class="team-info">
-                                <h3 class=" mb-1 h4">Ламиниране на вежди и мигли</h3>
+                        <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Масажи</h5>
+                        <p class="fs-110 font-cond-l" contenteditable="false">
+                            <p class=" mb-0">
+                                <ul>
 
-                            </div>
-                            <div class="team-overlay">
-                                <div class="d-table h-100 w-100">
-                                    <div class="d-table-cell align-middle">
-                                        <h3><a href="#" class="text-white">Ламиниране на вежди и мигли</a></h3>
-                                        <p class="text-white mb-0"><ul>  </ul>
-                                        </p>
-                                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                                    <li> Калсически</li>
+                                    <li>Силов</li>
+                                    <li>Възстановителен</li>
+                                    <li>Антицелулитен масаж с вендузи</li>
+                                </ul>
+                            </p></p>
+                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    </li>
+
+                    <li >
+                        <img src="img/viber_image_logo.jpg" class="wpx-100 img-round mgb-20" title="" alt="" data-edit="false" data-editor="field" data-field="src[Image Path]; title[Image Title]; alt[Image Alternate Text]" />
+
+                        <h5 class="font-cond mgb-5 fg-text-d fs-130" contenteditable="false">Ламиниране на вежди и мигли</h5>
+                        <p class="fs-110 font-cond-l" contenteditable="false">
+                            <p class=" mb-0">
+
+                            </p></p>
+                        <a href="/callery" className="btn btn-light" style={{}}>More</a>
+                    </li>
+                </ul>
+                
             </div>
+            
+        
 
             {/* <div className="row row-cols-1 row-cols-md-4 g-4  " style={{ marginTop: "0px", position: "absolute", left: "12%", paddingBottom: "300px" }}>
                 <div className="card" style={{ width: "24rem", marginRight: "20px" }}>
